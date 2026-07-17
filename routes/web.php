dbe02b13ef515b7d06137e22e6fef33801d77522
Civer Yoshioka <muarifin@pens.ac.id>
2026-07-17 17:43:20 +0700

    [P-2] setup routing resource semua tabel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
+namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/book', [BookController::class, 'index']);
+Route::get('/category', [CategoryController::class, 'index']);
+Route::resource('book', BookController::class);
